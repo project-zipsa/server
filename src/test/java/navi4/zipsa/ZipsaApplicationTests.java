bd79d00774f29a01e9b1b20f8e50506666e4a9bb
양효빈 <hyobin3026@naver.com>
2025-04-05 15:29:59 +0900

    [CHORE] 초기설정

---
+++ src/test/java/navi4/zipsa/ZipsaApplicationTests.java
@@ -0,0 +1,13 @@
+package navi4.zipsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
