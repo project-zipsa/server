5f7eb214d5abcc4f89d45425bc0a0e662a6ab73d
양효빈 <hyobin3026@naver.com>
2025-05-16 10:34:33 +0900

    [CHORE] flyway 스크립트 무시

--- src/main/resources/db/migration/V2__change_text_column_to_mediumtext.sql
+++
@@ -1,7 +0,0 @@
--- 1. jeonse_contract_text 컬럼을 MEDIUMTEXT로 변경
-ALTER TABLE contract_results
-    MODIFY COLUMN jeonse_contract_text MEDIUMTEXT;
-
--- 2. property_title_text 컬럼을 MEDIUMTEXT로 변경
-ALTER TABLE contract_results
-    MODIFY COLUMN property_title_text MEDIUMTEXT;
