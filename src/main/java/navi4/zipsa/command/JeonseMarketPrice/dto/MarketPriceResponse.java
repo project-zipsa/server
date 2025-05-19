e7203ba29f261c34ef3ccbb004640b60d4df963f
양효빈 <hyobin3026@naver.com>
2025-05-19 21:07:59 +0900

    [FEATURE] 시세정보 계산 로직 구현

--- src/main/java/navi4/zipsa/command/JeonseMarketPrice/dto/MarketPriceResponse.java
+++ src/main/java/navi4/zipsa/command/JeonseMarketPrice/dto/MarketPriceResponse.java
@@ -3,6 +3,7 @@ package navi4.zipsa.command.JeonseMarketPrice.dto;
 public record MarketPriceResponse(
         double averagePrice,
         double userPrice,
-        double differentPercent
+        double differentPercent,
+        double jeonseRatio
 ) {
 }
