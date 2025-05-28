641ca056022b082cb142a8d39455e1f31b87829e
양효빈 <hyobin3026@naver.com>
2025-05-28 13:14:17 +0900

    [FEATURE] 예외처리 추가

--- src/main/java/navi4/zipsa/command/JeonseContract/application/ContractService.java
+++ src/main/java/navi4/zipsa/command/JeonseContract/application/ContractService.java
@@ -7,10 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 import navi4.zipsa.command.JeonseContract.domain.ContractResult;
 import navi4.zipsa.command.JeonseContract.domain.ContractResultRepository;
 import navi4.zipsa.command.JeonseContract.domain.TotalContractAnalysisTemplate;
+import navi4.zipsa.common.exception.ExceptionMessages;
 import navi4.zipsa.infrastructure.api.gpt.application.GptApiService;
 import navi4.zipsa.infrastructure.api.odg.dto.TotalBrInfoRequest;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -21,7 +24,10 @@ public class ContractService {
     private final ObjectMapper objectMapper;
     private final GptApiService gptApiService; // TODO: 서비스 간 의존이 한 방향인지 재확인
 
-    private static final String NOT_FOUND_CONTRACT_RESULT = "계약서를 불러올 수 없습니다.";
+    private static final String NOT_FOUND_CONTRACT_RESULT = ExceptionMessages.ERROR_PREFIX + "계약서를 불러올 수 없습니다.";
+    private static final String NOT_FOUND_JEONSE_CONTRACT_JSON = ExceptionMessages.ERROR_PREFIX + "[Error]: 저장된 전세계약 정보가 없습니다.";
+    private static final String NOT_FOUND_PROPERTY_TITLE_JSON = ExceptionMessages.ERROR_PREFIX + "[Error]: 저장된 등기부등본 정보가 없습니다.";
+    private static final String FAIL_CONVERSION_BUILDING_REGISTER_TO_JSON = ExceptionMessages.ERROR_PREFIX + "건축물대장 JSON을 String으로 반환, 저장하는 데 실패했습니다";
 
     public ContractResult getContractResultsByContractId(Long contractId) {
         return contractResultRepository.findContractResultById(contractId)
@@ -38,8 +44,11 @@ public class ContractService {
         ContractResult contractResult = contractResultRepository.findContractResultsByUserId(userId)
                 .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_CONTRACT_RESULT));
 
-        String jeonseContractJson = contractResult.getJeonseContractJson();
-        String propertyTitleJson = contractResult.getPropertyTitleJson();
+        String jeonseContractJson = Optional.ofNullable(contractResult.getJeonseContractJson())
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_JEONSE_CONTRACT_JSON));
+
+        String propertyTitleJson = Optional.ofNullable(contractResult.getPropertyTitleJson())
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_PROPERTY_TITLE_JSON));
 
         return brInfoMono.flatMap(brInfo -> {
             try {
@@ -54,9 +63,8 @@ public class ContractService {
                         TotalContractAnalysisTemplate.PRINT_FORMAT;
                 return gptApiService.chat(prompt);
             } catch (JsonProcessingException e) {
-                return Mono.error(new IllegalArgumentException("건축물대장 JSON을 String으로 반환저장하는 로직 오류" + e.getMessage() + e));
+                return Mono.error(new IllegalArgumentException(FAIL_CONVERSION_BUILDING_REGISTER_TO_JSON + "\n" + e.getMessage() + e));
             }
         });
     }
-
 }
