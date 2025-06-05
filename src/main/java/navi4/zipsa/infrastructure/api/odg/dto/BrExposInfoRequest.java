94a4780f3f58d16207dad3078e1fd550bd77db0d
양효빈 <hyobin3026@naver.com>
2025-06-06 08:52:25 +0900

    [CHORE] 프론트 배포 주소 변경

--- src/main/java/navi4/zipsa/infrastructure/api/odg/dto/BrExposInfoRequest.java
+++ src/main/java/navi4/zipsa/infrastructure/api/odg/dto/BrExposInfoRequest.java
@@ -3,8 +3,8 @@ package navi4.zipsa.infrastructure.api.odg.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BrExposInfoRequest(
-        @JsonProperty("동") String dongNm, // 동
-        @JsonProperty("호") String hoNm, // 호
+        @JsonProperty("동") String dongNm,
+        @JsonProperty("호") String hoNm,
         @JsonProperty("층수") String flrNo
 ) {
 }
