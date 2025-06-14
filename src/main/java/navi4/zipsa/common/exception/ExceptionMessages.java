9b1e0c08aea3d3c14ae1245c5651c94e83db216e
양효빈 <hyobin3026@naver.com>
2025-06-14 16:00:18 +0900

    [FEATURE] 시세분석 시 유사조건이 없으면 204 status 반환

--- src/main/java/navi4/zipsa/common/exception/ExceptionMessages.java
+++ src/main/java/navi4/zipsa/common/exception/ExceptionMessages.java
@@ -1,7 +1,7 @@
 package navi4.zipsa.common.exception;
 
 public class ExceptionMessages {
-    public static final String ERROR_PREFIX = "[Error]: ";
-    public static final String SERVER_ERROR = "[Server Error]: 관리자에게 문의 바랍니다.";
-    public static final String INVALID_REQUEST = "[Request Error]: 유효하지 않은 요청입니다.";
+    public static final String ERROR_PREFIX = "[ERROR]: ";
+    public static final String SERVER_ERROR = "[SERVER ERROR]: 관리자에게 문의 바랍니다.";
+    public static final String INVALID_REQUEST = "[REQUEST ERROR]: 유효하지 않은 요청입니다.";
 }
